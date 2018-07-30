bff885932dc630eab8ab6e62ca07d8c582b56127
Fang <fang@fang.io>
2018-07-30 22:46:23 +0200

    Remove build scripts and results for asm.js (and PNaCl)

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 rm -rf dist && mkdir dist &&
-./clean-cmake.sh && ./build-asm.sh
 ./clean-cmake.sh && ./build-wasm.sh &&
-# ./clean-cmake.sh && ./build-pnacl.sh &&
 ./clean-cmake.sh
 echo Done
